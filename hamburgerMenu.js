7b7b3fe95beaf086a0e5673dae37c98b9e56e78b
showgo-nakajima <ebatech341@gmail.com>
2023-10-25 15:08:39 +0900

    ハンバーガーメニュー実装（indexのみ）

---
+++ hamburgerMenu.js
@@ -0,0 +1,12 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//   const hamburgerMenu = document.querySelector('.hamburger-menu');
+//   const hamburgerIcon = document.querySelector('.hamburger-icon');
+
+//   hamburgerIcon.addEventListener('click', () => {
+//       if (hamburgerMenu.style.right === '-250px') {
+//           hamburgerMenu.style.right = '0';
+//       } else {
+//           hamburgerMenu.style.right = '-250px';
+//       }
+//   });
+// });
